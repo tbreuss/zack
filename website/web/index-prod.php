d93083e3c89612313b6c61ea1c10143004f31215
tbreuss <thomasbreuss@gmx.ch>
2025-05-03 19:00:51 +0200

    fix prod environment

--- website/web/index-prod.php
+++ website/web/index-prod.php
@@ -1,10 +1,16 @@
 <?php declare(strict_types=1);
 
+ini_set('display_errors', '1');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use tebe\zack\Zack;
+use tebe\zack\Config;
 
-$config = require __DIR__ . '/config/config.php';
-$dispatcher = require __DIR__ . '/config/event-dispatcher.php';
+$config = new Config([
+    'rootPath' => dirname(__DIR__),
+    'twigCache' => false,
+]);
+
+$dispatcher = require dirname(__DIR__) . '/config/event-dispatcher.php';
 
 (new Zack($config, $dispatcher))->run();
