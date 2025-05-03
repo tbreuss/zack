69616812a1c2c4811859202521ae56573d7b0a90
tbreuss <thomasbreuss@gmx.ch>
2025-05-03 17:50:48 +0200

    add prod environment

---
+++ website/web/index-prod.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use tebe\zack\Zack;
+
+$config = require __DIR__ . '/config/config.php';
+$dispatcher = require __DIR__ . '/config/event-dispatcher.php';
+
+(new Zack($config, $dispatcher))->run();
