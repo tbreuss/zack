7c8b213520f24360824bc5b8a8c2bf79a48e7517
tbreuss <thomasbreuss@gmx.ch>
2025-05-26 20:15:17 +0200

    add phpstan

--- tests/_data/routes/articles/[id]/comments.php
+++ tests/_data/routes/articles/[id]/comments.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+/** @var tebe\zack\routing\PhpRouteHandler $this */
+
 return $this->html('articles/comments.html.twig', [
     'title' => 'GET: Articles.[id].Comments.Php',
 ]);
